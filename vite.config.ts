408403445aa639abca80ac7ad919afb218860e3b
Raymond Cox <raymondc1911@gmail.com>
2025-11-16 21:00:55 -0500

    chore: Add missing dependencies for nitro in package-lock.json

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite'
 import { devtools } from '@tanstack/devtools-vite'
 import { tanstackStart } from '@tanstack/react-start/plugin/vite'
+// @ts-expect-error Theres some issue with this packages version.
+// If you try to fix it, it breaks deployments
 import viteReact from '@vitejs/plugin-react'
 import viteTsConfigPaths from 'vite-tsconfig-paths'
 import tailwindcss from '@tailwindcss/vite'
